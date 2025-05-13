763d50b696491a8b01be3fa7b6c92f6878aa1a76
Miguel Angel Zapata Hinestroza <miguelzapata1114835@correo.itm.edu.co>
2025-05-12 21:16:19 -0500

    Avance en implementaciones e interfaces de lib_aplicaciones. 2 clases más

---
+++ lib_aplicaciones/Implementaciones/NivelesAplicacion.cs
@@ -0,0 +1,73 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    class AutoresAplicacion: IAutoresAplicacion
+    {
+        private Conexion conexion = new Conexion();
+
+        public List<Autores>? PorNombre(string nombre)
+        {
+            return this.conexion!.Autores!
+                .Where(x => x.Nombre!.Contains(nombre))
+                .ToList();
+        }
+
+        public Autores? Borrar(Autores entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            this.conexion!.Autores!.Remove(entidad);
+            this.conexion.SaveChanges();
+            return entidad;
+        }
+
+        public Autores? Guardar(Autores? entidad)
+        {            
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            this.conexion!.Autores!.Add(entidad);
+            this.conexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Autores> Listar()
+        {
+            return this.conexion!.Autores!.Take(20).ToList();
+        }
+
+        public Autores? PorId(int Id)
+        {
+            return this.conexion!.Autores!.FirstOrDefault(x => x.Id==Id);
+        }
+
+        public Autores? Modificar(Autores? entidad)
+
+
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            entidad.Apellido = "Apellido cambiado";
+
+            var entry = this.conexion!.Entry<Autores>(entidad);
+            entry.State = EntityState.Modified;
+            this.conexion.SaveChanges();
+            return entidad;
+        }
+    }
+}
---
+++ lib_aplicaciones/Interfaces/INiveles_tiene_LibrosAplicacion.cs
@@ -0,0 +1,15 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+
+namespace lib_aplicaciones.Interfaces
+{
+    interface IAutoresAplicacion
+    {       
+        Autores? PorId(int Id);
+        List<Autores>? PorNombre(string nombre);
+        List<Autores> Listar();
+        Autores? Guardar(Autores? entidad);
+        Autores? Modificar(Autores? entidad);
+        Autores? Borrar(Autores entidad);
+    }
+}

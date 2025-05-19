aa98b4013168c5cd90b56556beca4f4b6a476776
Miguel Angel Zapata Hinestroza <miguelzapata1114835@correo.itm.edu.co>
2025-05-19 15:49:39 -0500

    Subo CRUD prestamos

---
+++ asp_servicios/Controllers/PrestamosController.cs
@@ -0,0 +1,203 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_servicios.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PrestamosController : ControllerBase
+    {
+        private IPrestamosAplicacion? iPrestamosAplicacion = null;
+        private TokenController? tokenController = null;
+
+        public PrestamosController(IPrestamosAplicacion? iAplicacion,
+            TokenController tokenController)
+        {
+            this.iPrestamosAplicacion = iAplicacion;
+            this.tokenController = tokenController;
+        }
+
+        private Dictionary<string, object> ObtenerDatos()
+        {
+            var datos = new StreamReader(Request.Body).ReadToEnd().ToString();
+            if (string.IsNullOrEmpty(datos))
+                datos = "{}";
+            return JsonConversor.ConvertirAObjeto(datos);
+        }
+
+        [HttpPost]
+        public string Listar()
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+
+                respuesta["Entidades"] = this.iPrestamosAplicacion!.Listar();
+
+                respuesta["Respuesta"] = "OK";
+                respuesta["Fecha"] = DateTime.Now.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+
+        [HttpPost]
+        public string PorId() { 
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+
+                respuesta["Entidad"] = this.iPrestamosAplicacion!.PorId(Convert.ToInt32(datos["id"]));
+
+                respuesta["Respuesta"] = "OK";
+
+                respuesta["Fecha"] = DateTime.Now.ToString();
+
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+
+        [HttpPost]
+        public string PorFechaInicio()
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+
+                DateTime fechaInicio;
+                if (DateTime.TryParse(datos["FechaInicio"].ToString(), out fechaInicio))
+                {
+                    respuesta["Entidades"] = this.iPrestamosAplicacion!.PorFechaInicio(fechaInicio);
+                }
+                else
+                {
+                    respuesta["Error"] = "FechaInicio no es un formato válido.";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+                respuesta["Respuesta"] = "OK";
+                respuesta["Fecha"] = DateTime.Now.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+
+        [HttpPost]
+        public string Guardar()
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+
+                var entidad = JsonConversor.ConvertirAObjeto<Prestamos>(JsonConversor.ConvertirAString(datos["Entidad"]));
+
+                entidad = this.iPrestamosAplicacion!.Guardar(entidad);
+
+                respuesta["Entidad"] = entidad!;
+                respuesta["Respuesta"] = "OK";
+                respuesta["Fecha"] = DateTime.Now.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+
+        [HttpPost]
+        public string Modificar()
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+
+                var entidad = JsonConversor.ConvertirAObjeto<Prestamos>(JsonConversor.ConvertirAString(datos["Entidad"]));
+
+                entidad = this.iPrestamosAplicacion!.Modificar(entidad);
+
+                respuesta["Entidad"] = entidad!;
+                respuesta["Respuesta"] = "OK";
+                respuesta["Fecha"] = DateTime.Now.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+
+        [HttpPost]
+        public string Borrar()
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var datos = ObtenerDatos();
+                if (!tokenController!.Validate(datos))
+                {
+                    respuesta["Error"] = "lbNoAutenticacion";
+                    return JsonConversor.ConvertirAString(respuesta);
+                }
+                
+                var entidad = this.iPrestamosAplicacion!.Borrar(Convert.ToInt32(datos["id"]));
+
+                respuesta["Entidad"] = entidad!;
+                respuesta["Respuesta"] = "OK";
+                respuesta["Fecha"] = DateTime.Now.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.Message.ToString();
+                return JsonConversor.ConvertirAString(respuesta);
+            }
+        }
+    }
+}

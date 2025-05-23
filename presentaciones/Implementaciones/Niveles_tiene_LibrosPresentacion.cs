4c127f470e01fd70d78288e3482560c6ec5c4bb6
Miguel Angel Zapata Hinestroza <miguelzapata1114835@correo.itm.edu.co>
2025-05-23 16:50:21 -0500

    CRUD en lib_presentaciones de NumerosDeSerie, Niveles_tiene_Libros,Prestamos y TiposDocumentos

---
+++ presentaciones/Implementaciones/Niveles_tiene_LibrosPresentacion.cs
@@ -0,0 +1,118 @@
+﻿using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using presentaciones.Interfaces;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class Niveles_tiene_LibrosPresentacion : INiveles_tiene_LibrosPresentacion
+    {
+        private Comunicaciones? comunicaciones = null;
+
+        public async Task<List<Niveles_tiene_Libros>> Listar()
+        {
+            var lista = new List<Niveles_tiene_Libros>();
+            var datos = new Dictionary<string, object>();
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles_tiene_Libros/Listar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Niveles_tiene_Libros>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+
+            return lista;
+        }
+
+        public async Task<Niveles_tiene_Libros?> PorId(int Id)
+        {
+            var datos = new Dictionary<string, object>();
+            datos["Id"] = Id!;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles_tiene_Libros/PorId");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            Niveles_tiene_Libros? entidad = JsonConversor.ConvertirAObjeto<Niveles_tiene_Libros>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Niveles_tiene_Libros?> Guardar(Niveles_tiene_Libros? entidad)
+        {
+            if (entidad!.Id != 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles_tiene_Libros/Guardar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Niveles_tiene_Libros>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Niveles_tiene_Libros?> Modificar(Niveles_tiene_Libros? entidad)
+        {
+            if (entidad!.Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles_tiene_Libros/Modificar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Niveles_tiene_Libros>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Niveles_tiene_Libros?> Borrar(int Id)
+        {
+            if (Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Id"] = Id;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles_tiene_Libros/Borrar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            Niveles_tiene_Libros? entidad = JsonConversor.ConvertirAObjeto<Niveles_tiene_Libros>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
---
+++ presentaciones/Implementaciones/NumerosDeSeriePresentacion.cs
@@ -0,0 +1,137 @@
+﻿using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using presentaciones.Interfaces;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class NumerosDeSeriePresentacion : INumerosDeSeriePresentacion
+    {
+        private Comunicaciones? comunicaciones = null;
+
+        public async Task<List<NumerosDeSerie>> Listar()
+        {
+            var lista = new List<NumerosDeSerie>();
+            var datos = new Dictionary<string, object>();
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "NumerosDeSerie/Listar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<NumerosDeSerie>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+
+            return lista;
+        }
+        public async Task<List<NumerosDeSerie>?> PorNumeroSerie(string NumeroSerie)
+        {
+            var lista = new List<NumerosDeSerie>();
+            var datos = new Dictionary<string, object>();
+            datos["NumeroSerie"] = NumeroSerie!;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Niveles/PorNumeroSerie");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<NumerosDeSerie>>(
+               JsonConversor.ConvertirAString(respuesta["Entidades"]));
+
+            return lista;
+        }
+
+        public async Task<NumerosDeSerie?> PorId(int Id)
+        {
+            var datos = new Dictionary<string, object>();
+            datos["Id"] = Id!;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "NumerosDeSerie/PorId");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            NumerosDeSerie? entidad = JsonConversor.ConvertirAObjeto<NumerosDeSerie>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<NumerosDeSerie?> Guardar(NumerosDeSerie? entidad)
+        {
+            if (entidad!.Id != 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "NumerosDeSerie/Guardar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<NumerosDeSerie>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<NumerosDeSerie?> Modificar(NumerosDeSerie? entidad)
+        {
+            if (entidad!.Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "NumerosDeSerie/Modificar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<NumerosDeSerie>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<NumerosDeSerie?> Borrar(int Id)
+        {
+            if (Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Id"] = Id;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "NumerosDeSerie/Borrar");
+            var respuesta = await comunicaciones!.Execute(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            NumerosDeSerie? entidad = JsonConversor.ConvertirAObjeto<NumerosDeSerie>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}

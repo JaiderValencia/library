73690722704aef98be79d712b857a6dcd44e99f1
Miguel Angel Zapata Hinestroza <miguelzapata1114835@correo.itm.edu.co>
2025-06-02 19:40:49 -0500

    Pruebas Unitarias Tablas Restantes

---
+++ ut_presentacion/Repositorios/AccesosPrueba .cs
@@ -0,0 +1,56 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
+
+namespace ut_presentacion.Repositorios
+{
+    [TestClass]
+    public class AccesosPrueba
+    {
+        private readonly IConexion? iConexion = new Conexion();
+        private List<Accesos>? lista;
+        private Accesos? entidad;
+
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            Assert.AreEqual(true, Borrar());
+        }
+
+        public bool Listar()
+        {
+            this.lista = this.iConexion!.Accesos!.ToList();
+            return lista.Count > 0;
+        }
+
+        public bool Guardar()
+        {
+            this.entidad = EntidadesNucleo.Accesos()!;
+            this.iConexion!.Accesos!.Add(this.entidad);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+
+        public bool Modificar()
+        {
+            this.entidad!.Nombre = "PruebasCliente";
+
+            var entry = this.iConexion!.Entry<Accesos>(this.entidad);
+            entry.State = EntityState.Modified;
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+
+        public bool Borrar()
+        {
+            this.iConexion!.Accesos!.Remove(this.entidad!);
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+    }
+}
